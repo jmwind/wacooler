2cfe5eb3a48fa60091df5a6093bda7dec1a75c46
Jean-Michel Lemieux <jmwind@gmail.com>
2015-04-07 12:19:33 +1000

    Account activations

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     post   'login'   => 'sessions#create'
     delete 'logout'  => 'sessions#destroy'
     resources :users
+    resources :account_activations, only: [:edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-               admin: false)
+               admin: false,
+               activated: true,
+               activated_at: Time.zone.now)
 end
